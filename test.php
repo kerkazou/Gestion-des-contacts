8df329f67088494453532367c74ddc4d97939e64
kerkazou <kerkazou.zakaria@gmail.com>
2022-04-22 09:07:05 +0000

    ........

--- test.php
+++ test.php
@@ -1,24 +1,18 @@
-<form id="form-search" method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-    <span><span class="style2">Enter you email here</span>:</span>
-    <input name="email" type="email" id="email" required/>
-    <input type="submit" name="submit" value="subscribe"/>
+<form method="post" action="https://css-tricks.com/examples/DragAndDropFileUploading//?" enctype="multipart/form-data" novalidate class="box">
+    
+    <div class="box__input">
+        <svg class="box__icon" xmlns="http://www.w3.org/2000/svg" width="50" height="43" viewBox="0 0 50 43"><path d="M48.4 26.5c-.9 0-1.7.7-1.7 1.7v11.6h-43.3v-11.6c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v13.2c0 .9.7 1.7 1.7 1.7h46.7c.9 0 1.7-.7 1.7-1.7v-13.2c0-1-.7-1.7-1.7-1.7zm-24.5 6.1c.3.3.8.5 1.2.5.4 0 .9-.2 1.2-.5l10-11.6c.7-.7.7-1.7 0-2.4s-1.7-.7-2.4 0l-7.1 8.3v-25.3c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v25.3l-7.1-8.3c-.7-.7-1.7-.7-2.4 0s-.7 1.7 0 2.4l10 11.6z"/></svg>
+        <input type="file" name="files[]" id="file" class="box__file" data-multiple-caption="{count} files selected" multiple />
+        <label for="file"><strong>Choose a file</strong><span class="box__dragndrop"> or drag it here</span>.</label>
+        <button type="submit" class="box__button">Upload</button>
+    </div>
+
+    <div class="box__uploading">Uploading&hellip;</div>
+    <div class="box__success">Done! <a href="https://css-tricks.com/examples/DragAndDropFileUploading//?" class="box__restart" role="button">Upload more?</a></div>
+    <div class="box__error">Error! <span></span>. <a href="https://css-tricks.com/examples/DragAndDropFileUploading//?" class="box__restart" role="button">Try again!</a></div>
 </form>
 
 
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-<?php
-    if(isset($_POST['submit'])){
-        echo "
-            <script>
-                Swal.fire({
-                    position: 'top-end',
-                    icon: 'success',
-                    title: 'Your registration is successful',
-                    showConfirmButton: false,
-                    timer: 3000
-                })
-            </script>
-        ";
-    }
-    echo $_SERVER['REQUEST_URI'];
-?>
+<!-- <a href="/images/myw3schoolsimage.jpg" download>
+  <img src="/images/myw3schoolsimage.jpg" alt="W3Schools" width="104" height="142">
+</a> -->
